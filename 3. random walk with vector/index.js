1d729860ef5177defb53ea9575e4d8435f93a20b
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2021-10-22 15:40:48 +0900

    random work with vector
    radom walk 기능을 벡터를 사용해 구현

---
+++ 3. random walk with vector/index.js
@@ -0,0 +1,12 @@
+let walker;
+
+function setup() {
+    createCanvas(400, 400);
+    background(0);
+    walker = new Walker(200, 200);
+}
+
+function draw() {
+    walker.update();
+    walker.show();
+}
